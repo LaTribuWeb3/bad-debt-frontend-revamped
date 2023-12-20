b33839acd59c7748378219fb7560f7dd90275b27
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-12-20 15:59:11 +0100

    responsive background, text and theme switcher

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import TopBar from "./components/ui/TopBar";
 export default function Home() {
   return (
     <div>
-      <ThemeSwitch />
-
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <TopBar />
+      <ThemeSwitch />
+    <main className="flex bg-white dark:bg-darkThemeBg min-h-screen text-black dark:text-white flex-col items-center justify-between p-24">
       test
     </main>
     </div>
