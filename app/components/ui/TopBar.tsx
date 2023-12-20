d311bca92f2f7b9d154cf9fb8705855a7de9a3bf
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-12-20 15:45:13 +0100

    top bar done

---
+++ app/components/ui/TopBar.tsx
@@ -0,0 +1,20 @@
+import { FaMediumM } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoLogoDiscord } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
+export default function TopBar(){
+    return <div className="fixed top-0 right-0 p-2 md:p-4 flex space-x-1 md:space-x-4">
+        <a href="https://twitter.com/risk_dao" target="_blank" rel="noopener noreferrer">
+        <FaSquareXTwitter className="text-gray-900 dark:text-gray-200 text-3xl" />
+        </a>
+        <a href="https://discord.com/invite/NYyeDQDDvM" target="_blank" rel="noopener noreferrer">
+        <IoLogoDiscord className="text-gray-900 dark:text-gray-200 text-3xl" />
+        </a>
+        <a href="https://medium.com/risk-dao" target="_blank" rel="noopener noreferrer">
+        <FaMediumM className="text-gray-900 dark:text-gray-200 text-3xl" />
+        </a>
+        <a href="https://github.com/Risk-DAO/" target="_blank" rel="noopener noreferrer">
+        <FaGithub className="text-gray-900 dark:text-gray-200 text-3xl" />
+        </a>
+    </div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import ThemeSwitch from "./components/ui/ThemeSwitch";
+import TopBar from "./components/ui/TopBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ThemeSwitch />
+      <TopBar />
       test
     </main>
   )
