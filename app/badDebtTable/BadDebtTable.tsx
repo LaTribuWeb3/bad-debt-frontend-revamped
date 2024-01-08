4c83e652bd77c2a8e36bbd8c546151843a69eb05
0xVorian <33686588+0xVorian@users.noreply.github.com>
2024-01-08 11:41:48 +0100

    added dummy data for on the move work

--- app/badDebtTable/BadDebtTable.tsx
+++ app/badDebtTable/BadDebtTable.tsx
@@ -1,9 +1,10 @@
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "./columns";
 import DataService from "@/lib/dataService";
+import { dummyData } from "@/lib/dummyData";
 
 export default async function MarketDataTable(){
-    const data = await DataService.GetProtocolResults('latest');
+    const data = dummyData;
 
     return (
         <div className="container mx-auto py-10">
